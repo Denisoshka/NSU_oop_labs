fd4682155970f8a6a4645a91118d69a5c9134f52
denis <d.zhdanov@g.nsu.ru>
2023-09-09 11:13:47 +0700

    delete other project files

--- sound_processor/cl_parser/src/cl_parser.cpp
+++
@@ -1,9 +0,0 @@
-#include "cl_parser.h"
-
-void clParser::getOptions() {
-  po::options_description opts_desc("General options");
-    opts_desc.add_options()
-        ("help,h",
-         "Show options description")
-        ("config,c",
-}
