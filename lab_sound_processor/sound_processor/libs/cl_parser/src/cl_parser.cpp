8325283476552778c89a870df0e0ce8e467d5915
Denis <dinis.04@yandex.ru>
2023-10-17 13:56:03 +0700

    fix cl_parser

--- lab_sound_processor/sound_processor/libs/cl_parser/src/cl_parser.cpp
+++ lab_sound_processor/sound_processor/libs/cl_parser/src/cl_parser.cpp
@@ -10,9 +10,9 @@ bool CLParser::parseOptions(const int kArgc, char **argv,
   po::options_description desc("General options");
   std::string task_type;
   desc.add_options()("help,h", "Show options description")(
-          "config,C", po::value<std::string>()->required(), "Configuration file")(
-          "output,O", po::value<std::string>()->required(), "Output file")(
-          "input,I", po::value<std::vector<std::string>>()->multitoken()->required(),
+          "config,C", po::value<std::string>(), "Configuration file")(
+          "output,O", po::value<std::string>(), "Output file")(
+          "input,I", po::value<std::vector<std::string>>()->multitoken(),
           "Input files");
 
   po::parsed_options parsed =
@@ -20,7 +20,7 @@ bool CLParser::parseOptions(const int kArgc, char **argv,
   po::store(parsed, vm);
   po::notify(vm);
 
-  if( vm.count("help") ) {
+  if( vm.count("help") || !vm.count("input") || !vm.count("output") ||  !vm.count("config") ) {
     return false;
   }
   else {
